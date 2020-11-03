c6bf0112eaf9ab0821d06d0d07f1faea33199aa0
mhDu <695948191@qq.com>
2020-11-03 08:48:41 +0800

    R to RMarkdown with md

--- C19_Render an R script.R
+++ C19_Render an R script.R
@@ -1,3 +1,10 @@
+#' ---
+#' title: "R scripts can be rendered!"
+#' author: "Minghao Du"
+#' date: "November 3, 2020"
+#' output: github_document
+#' ---
+#'
 #' Here's some prose in a very special comment. Let's summarize the built-in
 #' dataset `VADeaths`.
 ## here is a regular code comment, that will remain as such
